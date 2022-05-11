8ea4d4751422a8677df4a358606aa14cdbb07b8e
lyq <1661898579@qq.com>
2022-05-11 10:01:31 +0800

    formlogin模式登录认证

--- basic-server/src/main/java/com/zimug/courses/security/basic/config/SecurityConfig.java
+++ basic-server/src/main/java/com/zimug/courses/security/basic/config/SecurityConfig.java
@@ -1,21 +1,66 @@
 package com.zimug.courses.security.basic.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        开启httpbasic认证
-        http.httpBasic()
+        http.csrf().disable()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .and()
+                .authorizeRequests()
+//                不需要通过登录验证就可以被访问的资源
+                .antMatchers("/login.html","/login").permitAll()
+//                资源路径匹配
+                .antMatchers("/","/biz1","/biz2")
+//                user角色和admin角色都可以访问
+                .hasAnyAuthority("ROLE_user","ROLE_admin")
+                .antMatchers("/syslog","/sysuser")
+                .hasAnyRole("admin")
+                .anyRequest()
+                .authenticated();
+
+
+/*//        开启httpbasic认证
+                .httpBasic()
                 .and()
                 .authorizeRequests()
                 .anyRequest()
 //                所有请求需要登录认证
-                .authenticated();
+                .authenticated();*/
 //        httpBasic认证不安全 可以在网页对其进行解密
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("123456"))
+                .roles("user")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("123456"))
+                .roles("admin")
+                .and()
+//                配置BCrypt加密
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
